9e8bc0c8d09c08386d3ea2967a7a751262e7e63e
Navanit Vishwakarma <navanitvishwakarma790@gmail.com>
2025-04-24 11:43:12 +0530

    new chnages done

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,7 +5,7 @@ const admin = localStorage.getItem("admin");
 console.log( "this token",admin);
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4500', // Replace with your base URL
+  baseURL: 'https://astrovani-6d54b00db2da.herokuapp.com/', // Replace with your base URL
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${admin}`,
